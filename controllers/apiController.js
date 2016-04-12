1537b2a04169dd2d8d792717bb29bafccc6f3dbb
AdrianaVillagran <anvillagran1@gmail.com>
2016-04-12 02:36:09 -0700

    re-oriented api route

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -1,15 +1,17 @@
 function index(req, res) {
   res.json({
-    message: "Welcome to tunely!",
-    documentation_url: "https://github.com/tgaff/tunely/api.md",
-    base_url: "http://tunely.herokuapp.com",
+    message: "Welcome to T.I.L.",
+    documentation_url: "https://github.com/AdrianaVillagran/til-app",
+    base_url: "https://whispering-earth-61253.herokuapp.com/",
     endpoints: [
-      {method: "GET", path: "/api", description: "Describes available endpoints"}, //done
-      {method: "GET", path: "/api/bows", description: "Shows all beads of wisdom posted by users"},//done
-      {method: "GET", path: "/api/bows/:date", description: "Shows beads by date"},
-      {method: "POST", path: "/api/bows", description: "Allows a user to create a bead of wisdom"}, //done
-      {method: "PUT", path: "/api/bows/:id", description: "Allows a user to update a bead of wisdom"}, //done
-      {method: "DELETE", path: "/api/bows/:id", description: "Allows a user to delete a bead of wisdom"} //done
+      {method: "GET", path: "/api", description: "Describes available endpoints"},
+      {method: "GET", path: "/api/bows", description: "Shows all beads of wisdom posted by all users"},
+      {method: "GET", path: "/api/users", description: "Shows all users"},
+      {method: "GET", path: "/api/users/:username/bows", description: "Shows all beads of wisdom by a user"},
+      {method: "GET", path: "/api/users/:username/bows/:date", description: "Shows all beads of wisdom posted by a user posted on a specific date"},
+      {method: "POST", path: "/api/users/:username/bows", description: "Allows a user to create a bead of wisdom"},
+      {method: "PUT", path: "/api/users/:username/bows/:id", description: "Allows a user to update a bead of wisdom"},
+      {method: "DELETE", path: "/api/users/:username/bows/:id", description: "Allows a user to delete a bead of wisdom"}
     ]
   });
 }
